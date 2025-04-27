130725ce87691e401f4b540732154d0217f9be54
Semih Yeşilyurt <semih.yesilyurt@gmail.com>
2025-04-27 20:31:32 +0300

    text update

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Well done! You have successfully deployed your Medusa 2.0 store on Railway!
+            Well Well Well! You have successfully deployed your Medusa 2.0 store on Railway!
           </Heading>
           <Heading
             level="h2"
@@ -18,15 +18,7 @@ const Hero = () => {
           >
             Need help customizing your store?
           </Heading>
-        </span>
-        <a
-          href="https://funkyton.com/medusajs-2-0-is-finally-here/"
-          target="_blank"
-        >
-          <h1 style={{ textDecoration: "underline" }}>
-            Visit the tutorial
-          </h1>
-        </a>
+        </span>        
       </div>
     </div>
   )
